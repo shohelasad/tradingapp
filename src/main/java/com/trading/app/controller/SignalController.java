58e27d57df9f050c406f7cfc148c6760af914e16
shohelasad <shohel.asad@gmail.com>
2023-11-06 00:33:43 +0100

    add creating signal service with rest api and postgres jsonb data

--- src/main/java/com/trading/app/controller/SignalController.java
+++ src/main/java/com/trading/app/controller/SignalController.java
@@ -2,13 +2,14 @@ package com.trading.app.controller;
 
 import com.trading.app.dto.SignalSpec;
 import com.trading.app.entity.Signal;
-import com.trading.app.service.SignalHandlerImpl;
 import com.trading.app.service.SignalService;
-import com.trading.app.service.SignalHandler;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
@@ -17,20 +18,8 @@ import org.springframework.web.bind.annotation.*;
 public class SignalController {
     private final SignalService signalService;
 
-    private final SignalHandler signalHandler;
-
-    public SignalController(SignalService signalServiceImpl, SignalHandlerImpl signalHandlerImpl) {
+    public SignalController(SignalService signalServiceImpl) {
         this.signalService = signalServiceImpl;
-        this.signalHandler = signalHandlerImpl;
-    }
-
-    @PostMapping("/{signalId}")
-    public ResponseEntity<String> sendSignal(@PathVariable int signalId) {
-        if (signalId == 0) {
-            throw new RuntimeException("Signal Id cannot be negative!");
-        }
-        signalHandler.handleSignal(signalId);
-        return ResponseEntity.ok().build();
     }
 
     @PostMapping
